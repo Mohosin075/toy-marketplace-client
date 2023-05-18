abc0183c77ce73eb0dd9cf6116eb905afaf3e64c
Md Alim <mdalimoattabur@gmail.com>
2023-05-19 01:23:03 +0600

    register page design

--- toy-marketplace-client/src/pages/login/Login.jsx
+++ toy-marketplace-client/src/pages/login/Login.jsx
@@ -9,30 +9,29 @@ const Login = () => {
         <h2 className="text-5xl font-bold">Please <span className="text-blue-600 font-bold">Login</span>!</h2>
         <div className="card flex-shrink-0 w-full max-w-lg border border-1 shadow-lg bg-base-100">
           <form className="card-body">
-            <div className="form-control">
+          <div className="form-control">
               <label className="label">
-                <span className="label-text">Email</span>
+                <span className="label-text">Email :</span>
               </label>
               <input
-                type="text"
+              name="email"
+                type="email"
                 placeholder="email"
                 className="input input-bordered"
+                required
               />
             </div>
             <div className="form-control">
               <label className="label">
-                <span className="label-text">Password</span>
+                <span className="label-text">Password :</span>
               </label>
               <input
-                type="text"
+              name="password"
+                type="password"
                 placeholder="password"
                 className="input input-bordered"
+                required
               />
-              <label className="label">
-                <a href="#" className="label-text-alt link link-hover">
-                  Forgot password?
-                </a>
-              </label>
             </div>
             <div className="form-control mt-6">
               <button className="btn btn-primary btn-outline">Login</button>
--- toy-marketplace-client/src/pages/register/Register.jsx
+++ toy-marketplace-client/src/pages/register/Register.jsx
@@ -1,11 +1,71 @@
+import { Link } from "react-router-dom";
 import useTitle from "../hooks/useTitle";
 
 const Register = () => {
     useTitle('register')
     return (
-        <div>
-            this is register
+        <div className="hero my-12 md:my-20 w-full">
+      <div className="hero-content flex-col w-full">
+        <h2 className="text-5xl font-bold">Please <span className="text-blue-600 font-bold">Register</span>!</h2>
+        <div className="card flex-shrink-0 w-full max-w-lg border border-1 shadow-lg bg-base-100">
+          <form className="card-body">
+            <div className="form-control">
+              <label className="label">
+                <span className="label-text">Name :</span>
+              </label>
+              <input
+              name="name"
+                type="text"
+                placeholder="Name"
+                className="input input-bordered"
+                required
+              />
+            </div>
+            <div className="form-control">
+              <label className="label">
+                <span className="label-text">Email :</span>
+              </label>
+              <input
+              name="email"
+                type="email"
+                placeholder="email"
+                className="input input-bordered"
+                required
+              />
+            </div>
+            <div className="form-control">
+              <label className="label">
+                <span className="label-text">Password :</span>
+              </label>
+              <input
+              name="password"
+                type="password"
+                placeholder="password"
+                className="input input-bordered"
+                required
+              />
+            </div>
+            <div className="form-control">
+              <label className="label">
+                <span className="label-text">Photo URL :</span>
+              </label>
+              <input
+              name="photo"
+                type="url"
+                placeholder="Photo URL"
+                className="input input-bordered"
+                required
+              />
+            </div>
+            <div className="form-control mt-6">
+              <button className="btn btn-primary btn-outline">Register</button>
+            </div>
+            <p className="text-sm mt-3">Already have an Account ? Please  <Link to="/login" className="text-blue-700 underline hover:text-blue-600">Login</Link></p>
+          </form>
+          
         </div>
+      </div>
+    </div>
     );
 };
 
